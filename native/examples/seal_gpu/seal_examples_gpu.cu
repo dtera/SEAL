4bea3517e8e9207d1e33be5c8a65197a01a9bf28
dtera <huster1005@gmail.com>
2025-01-22 16:16:20 +0800

    fix: reassign complex type instead of double for plaintext

--- native/examples/seal_gpu/seal_examples_gpu.cu
+++ native/examples/seal_gpu/seal_examples_gpu.cu
@@ -33,19 +33,19 @@ int main()
 
     /* 使用 CKKSEncoder 批处理并编码 */
     CKKSEncoder encoder(context);
-    auto slots = encoder.slotCount() * 2;
+    auto slots = encoder.slotCount() * 1;
     /* 设置 scale 缩放的值 */
     double scale = pow(2.0, 40);
 
     /* 初始化向量 */
-    vector<double> vx1(slots, 0.0), vx2(slots, 0.0), v1(slots, 0.0), v2(slots, 0.0);
-    double real = 0;
+    vector<complex<double>> vx1(slots, 0.0), vx2(slots, 0.0), v1(slots, 0.0), v2(slots, 0.0);
+    complex<double> real = 0;
     int n = slots / 8;
     for (int i = 0; i < n; i++)
     {
         vx1[i] = i + 1;
         vx2[i] = 2 * i + 1;
-        double t = vx1[i] * vx2[i];
+        complex<double> t = vx1[i] * vx2[i];
         if (i < n)
         {
             real += t * t;
@@ -60,29 +60,29 @@ int main()
     /* 编码并加密 */
     Plaintext plain_vx1, plain_vx2, pt1, pt2;
     Ciphertext encrypted_vx1, encrypted_vx2;
-    encoder.encodePolynomial(vx1, scale, plain_vx1);
-    encoder.encodePolynomial(vx2, scale, plain_vx2);
+    encoder.encode(vx1, scale, plain_vx1);
+    encoder.encode(vx2, scale, plain_vx2);
     encryptor.encrypt(plain_vx1, encrypted_vx1);
     encryptor.encrypt(plain_vx2, encrypted_vx2);
 
     decryptor.decrypt(encrypted_vx1, pt1);
-    encoder.decodePolynomial(pt1, v1);
+    encoder.decode(pt1, v1);
     cout << "解密向量vx1:";
     print_vector(v1);
     decryptor.decrypt(encrypted_vx2, pt2);
-    encoder.decodePolynomial(pt2, v2);
+    encoder.decode(pt2, v2);
     cout << "解密向量vx2:";
     print_vector(v2);
 
     Ciphertext add, sub;
     evaluator.add(encrypted_vx1, encrypted_vx2, add);
     decryptor.decrypt(add, pt1);
-    encoder.decodePolynomial(pt1, v1);
+    encoder.decode(pt1, v1);
     cout << "解密向量vx1 + vx2:";
     print_vector(v1);
     evaluator.sub(encrypted_vx2, encrypted_vx1, sub);
     decryptor.decrypt(sub, pt2);
-    encoder.decodePolynomial(pt2, v2);
+    encoder.decode(pt2, v2);
     cout << "解密向量vx2 - vx1:";
     print_vector(v2);
 
@@ -93,14 +93,14 @@ int main()
     evaluator.relinearizeInplace(mul, relin_keys);
     evaluator.rescaleToNextInplace(mul);
     decryptor.decrypt(mul, pt1);
-    encoder.decodePolynomial(pt1, v1);
+    encoder.decode(pt1, v1);
     cout << "解密向量vx1 * vx2:";
     print_vector(v1);
     evaluator.squareInplace(mul);
     evaluator.relinearizeInplace(mul, relin_keys);
     evaluator.rescaleToNextInplace(mul);
     decryptor.decrypt(mul, pt2);
-    encoder.decodePolynomial(pt2, v2);
+    encoder.decode(pt2, v2);
     cout << "解密向量(vx1 * vx2)^2:";
     print_vector(v2);
 
@@ -116,9 +116,9 @@ int main()
     /* 解密 */
     /* 将密文解密解码至 vector */
     Plaintext total_plain;
-    vector<double> result;
+    vector<complex<double>> result;
     decryptor.decrypt(total, total_plain);
-    encoder.decodePolynomial(total_plain, result);
+    encoder.decode(total_plain, result);
 
     /* 打印结果 */
     cout << endl << "解密数组: ";
